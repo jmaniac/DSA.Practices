a133143ea836e46b7868a2eb1f25f14f8dd99317
Sathish Guru <guru@joshbrains.com>
2025-09-14 22:29:57 +0530

    Trying Benchmarks.NET for the sorting

---
+++ Practices.Benchmark.Runner/Data/Helper.cs
@@ -0,0 +1,17 @@
+﻿using Bogus;
+
+namespace Practices.Benchmark.Runner.Data
+{
+    internal static class DataHelper
+    {
+
+        public static int[] GetRandomArray()
+        {
+            Randomizer.Seed = new Random();
+
+            var faker = new Faker();
+
+            return Enumerable.Range(1, int.MaxValue).Select(_ => faker.Random.Int(1)).ToArray();
+        }
+    }
+}
---
+++ Practices.Benchmark.Runner/SortingBenchMarks.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using Practices.Algorithms.Sorting;
+
+namespace Practices.Benchmark.Runner
+{
+    public class SortingBenchMarks
+    {
+        [Benchmark(OperationsPerInvoke = 5)]
+        public void SelectionSorting()
+        {
+            var array = new[] { 3, 5, 2, 1, 4 };
+            SelectionSort.Sort(array);
+        }
+
+
+        [Benchmark(OperationsPerInvoke = 5)]
+        public void BubbleSorting()
+        {
+            var array = new[] { 3, 5, 2, 1, 4 };
+            BubbleSort.Sort(array);
+        }
+    }
+}
