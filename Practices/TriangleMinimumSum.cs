3c576ed948e21df8e7d00ccbfa6022a2a08b13c1
jmaniac <sathishguru@gmail.com>
2025-09-25 09:56:28 +0530

    120. Triangle

---
+++ Practices/TriangleMinimumSum.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practices.Problems
+{
+    internal class TriangleMinimumSum
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            int rowCount = triangle.Count;
+
+            if (rowCount == 0) return 0;
+
+            for (int row = 1; row < triangle.Count; row++)
+            {
+                for (int col = 0; col <= row; col++)
+                {
+                    if (col == 0)
+                    {
+                        triangle[row][col] += triangle[row - 1][col];
+                    }
+                    else if (col == row)
+                    {
+                        triangle[row][col] += triangle[row - 1][col - 1];
+                    }
+                    else
+                    {
+                        triangle[row][col] += Math.Min(triangle[row - 1][col], triangle[row - 1][col - 1]);
+                    }
+                }
+            }
+
+            return triangle.Last().Min();
+        }
+    }
+}
