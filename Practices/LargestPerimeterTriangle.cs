935d82de3bd339a1e3af842cd0ca863217d29b45
jmaniac <sathishgurucse@gmail.com>
2025-09-28 12:06:21 +0530

    976. Largest Perimeter Triangle

---
+++ Practices/LargestPerimeterTriangle.cs
@@ -0,0 +1,23 @@
+﻿namespace Practices.Problems
+{
+    internal class LargestPerimeterTriangle
+    {
+        public int LargestPerimeter(int[] nums)
+        {
+            int length = nums.Length;
+
+            if (length < 3) return 0;
+
+            // Reverse Sort aka Descending Order
+            Array.Sort(nums, Comparer<int>.Create((a, b) => b - a));
+
+            for (int i = 0; i < length - 2; i++)
+            {
+                if (nums[i] < nums[i + 1] + nums[i + 2])
+                    return nums[i] + nums[i + 1] + nums[i + 2];
+            }
+
+            return 0;
+        }
+    }
+}
