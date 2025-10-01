a451ae824b96393ac81fb6343f62f9b51732a0ed
jmaniac <sathishguru@gmail.com>
2025-10-01 10:10:52 +0530

    2221. Find Triangular Sum of an Array

---
+++ Practices/ArrayTriangularSum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Practices.Problems
+{
+    internal class ArrayTriangularSum
+    {
+        public int TriangularSum(int[] nums)
+        {
+            if (nums.Length == 1) return nums[0];
+
+            int[] result = new int[nums.Length - 1];
+
+            for (int i = 0; i < nums.Length -1; i++)
+            {
+                result[i] = (nums[i] + nums[i+1]) % 10;
+            }
+
+            return TriangularSum(result);
+        }
+    }
+}
