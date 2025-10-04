8a646c86833e4e47a59640b5da25ad98fdb35045
jmaniac <sathishgurucse@gmail.com>
2025-10-04 20:02:43 +0530

    11. Container With Most Water

---
+++ Practices/ContainerWithMostWater.cs
@@ -0,0 +1,22 @@
+﻿namespace Practices.Problems
+{
+    internal class ContainerWithMostWater
+    {
+        public int MaxArea(int[] height)
+        {
+            int maxArea = 0;
+
+            for (int left = 0, right = height.Length - 1; left < right;)
+            {
+                int currentArea = Math.Min(height[left], height[right]) * (right - left);
+                maxArea = Math.Max(maxArea, currentArea);
+                if (height[left] < height[right])
+                    left++;
+                else
+                    right--;
+            }
+
+            return maxArea;
+        }
+    }
+}
