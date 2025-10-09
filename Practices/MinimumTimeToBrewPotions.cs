f6c1e73510dd35dedb3d475bca3747005333ed0c
jmaniac <sathishguru@gmail.com>
2025-10-09 10:25:32 +0530

    3494. Find the Minimum Amount of Time to Brew Potions

---
+++ Practices/MinimumTimeToBrewPotions.cs
@@ -0,0 +1,26 @@
+﻿namespace Practices.Problems
+{
+    internal class MinimumTimeToBrewPotions
+    {
+        public long MinTime(int[] skill, int[] mana)
+        {
+            int n = skill.Length, m = mana.Length;
+            long[] times = new long[n];
+
+            for (int j = 0; j < m; j++)
+            {
+                long curTime = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    curTime = Math.Max(curTime, times[i]) + (long)skill[i] * mana[j];
+                }
+                times[n - 1] = curTime;
+                for (int i = n - 2; i >= 0; i--)
+                {
+                    times[i] = times[i + 1] - (long)skill[i + 1] * mana[j];
+                }
+            }
+            return times[n - 1];
+        }
+    }
+}
