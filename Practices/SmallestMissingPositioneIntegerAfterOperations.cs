f4e6da37939e68ada3ac4a10d62d4867ac344099
jmaniac <sathishguru@gmail.com>
2025-10-16 10:23:28 +0530

    2598. Smallest Missing Non-negative Integer After Operations

---
+++ Practices/SmallestMissingPositioneIntegerAfterOperations.cs
@@ -0,0 +1,18 @@
+﻿namespace Practices.Problems
+{
+    internal class SmallestMissingPositioneIntegerAfterOperations
+    {
+        public int FindSmallestInteger(int[] nums, int value)
+        {
+            int n = nums.Length, res = 0;
+            int[] rem = new int[value];
+            foreach (int x in nums)
+            {
+                int r = ((x % value) + value) % value;
+                rem[r]++;
+            }
+            while (rem[res % value]-- > 0) res++;
+            return res;
+        }
+    }
+}
