28af9a00faea73383a0f7f865253defbc4eb77f8
jmaniac <sathishguru@gmail.com>
2025-10-25 19:08:32 +0530

    1716. Calculate Money in Leetcode Bank

---
+++ Practices/CalculateMoneyinLeetcodeBank.cs
@@ -0,0 +1,16 @@
+﻿namespace Practices.Problems
+{
+    internal class CalculateMoneyinLeetcodeBank
+    {
+        public int TotalMoney(int n)
+        {
+            int total = 0, nDays = 7;
+            for (int i = 1; i <= n; i++)
+            {
+                int rem = i % nDays, q = i / nDays;
+                total += (q + (rem == 0 ? nDays - 1 : rem));
+            }
+            return total;
+        }
+    }
+}
